0830d653153e6144b58b63793611c96bf1fd1dee
Mahklvk <ranggasidikp@proton.me>
2026-04-03 11:07:34 +0700

    add: search bar

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,17 @@
 import { ThemedText } from "@/components/themed-text";
+import { StyleSheet } from "react-native";
 import "../../../input.css";
 
-import { View } from "react-native";
+import { View, TextInput } from "react-native";
 export function Header() {
-    
   return (
     <View className="sticky">
-      <ThemedText type="title" className="text-center">
-        Moviescx
+      <ThemedText className="text-center h-15" type="title">
+        MOVIEScx.
       </ThemedText>
+      <View className="border-2 border-green-200 me-2 ms-2">
+            <TextInput placeholder="Search movie..." className="text-white" />
+          </View>
     </View>
   );
 }
