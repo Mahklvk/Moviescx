118a9e129dc64a68b64198d27332bd2529f646bb
Mahklvk <ranggasidikp@proton.me>
2026-04-03 11:07:00 +0700

    change: modular

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,61 +1,45 @@
 import { ThemedText } from "@/components/themed-text";
-import { ThemedView } from "@/components/themed-view";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../../input.css";
 
-import { Trending } from "@/api/client";
-import Movie from "@/interface/movies";
-import { useFocusEffect } from "@react-navigation/native";
-import React, { useCallback, useState } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
-import { Image } from "expo-image";
 import { Header } from "@/components/ui/header";
+import { Filters } from "@/components/ui/trending";
 import { useTheme } from "@/hooks/use-theme";
-import { RenderTrending } from "@/components/ui/render-trending";
+import React, { useCallback } from "react";
+
+import { LinearGradient } from "expo-linear-gradient";
+import { ScrollView, View } from "react-native";
+
+import { Carousels } from "@/components/ui/carousel";
 
 export default function HomeScreen() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-
-  useFocusEffect(
-    useCallback(() => {
-      fetchMovies();
-    }, []),
-  );
-
-  const fetchMovies = async () => {
-    try {
-      // setIsLoading(true);
-      const response = await Trending.Movies();
-      
-      const movies = response.results;
-
-      setMovies(movies);
-    } catch (error) {
-      console.error("Fetch quizzes error:", error);
-      // Alert.alert('Error', 'Gagal memuat data quiz');
-    } finally {
-      // setIsLoading(false);
-    }
-  };
-
-  const renderTrending = ({ item }: { item: Movie }) => (
-  <RenderTrending item={item} />
-);
   const theme = useTheme();
 
   return (
-    <SafeAreaView style={[{ backgroundColor: theme.background }]}>
-      <Header />
-      <FlatList
-        data={movies}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem= {renderTrending}
-        className="px-5"
-        horizontal={true}
-        initialNumToRender={2}
-        removeClippedSubviews={true}
-      />
+    <ScrollView>
+      <SafeAreaView style={[{ backgroundColor: theme.background }]}>
+        <Header />
+        <Carousels />
 
-    </SafeAreaView>
+        <View style={{ backgroundColor: "#363a4f" }}>
+          <LinearGradient
+            colors={["transparent", "rgba(0,0,0,0.8)"]}
+            style={{
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              right: 0,
+              height: 400,
+            }}
+          />
+          <View>
+            <View>
+              <ThemedText className="text-center mt-5">Trending</ThemedText>
+            </View>
+          </View>
+          <Filters />
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
