77bc8d588a220b1c097f8520d78d42b38cdd896c
Mahklvk <ranggasidikp@proton.me>
2026-04-03 11:09:28 +0700

    add: tv interfaces

---
+++ src/interface/tv.ts
@@ -0,0 +1,17 @@
+import { Double } from "react-native/Libraries/Types/CodegenTypes";
+
+export default interface Tv {
+    'adult': boolean,
+    'id': number,
+    'backdrop_path': string,
+    'poster_path': string,
+    'original_language': string,
+    'original_name': string,
+    'overview': string,
+    'media_type': string,
+    'genre_ids': string[],
+    'popularity': number,
+    'release_date': Date,
+    'vote_average': Double,
+    'vote_count': number,
+}
