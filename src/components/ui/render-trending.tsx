651c125dcbe57caed42b7b647157ecb8ccae033c
Mahklvk <ranggasidikp@proton.me>
2026-04-03 11:08:43 +0700

    change: selection for movie or  tv

--- src/components/ui/render-trending.tsx
+++ src/components/ui/render-trending.tsx
@@ -1,29 +1,31 @@
 import Movie from "@/interface/movies";
+import Tv from "@/interface/tv";
 import { Image } from "expo-image";
+import React from "react";
 import { StyleSheet } from "react-native";
 import { ThemedText } from "../themed-text";
 import { ThemedView } from "../themed-view";
-import React from "react";
+
+type Media = Movie | Tv
+
 
 type Props = {
-  item: Movie;
+  item: Media;
 };
 
-export const RenderTrending: React.FC<Props> = ({ item }) => {
-    return (
-        <ThemedView className="items-center" style={styles.card}>
-            <Image
-                source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
-                style={styles.poster}
-            />
-            <ThemedText type="link" className="text-center" style={styles.title}>
-                {item.original_title}
-            </ThemedText>
-            {/* <ThemedText>{item.vote_average}</ThemedText>
-          <ThemedText>{item.genre_ids}</ThemedText> */}
-        </ThemedView>
-    );
-}
+export const RenderTrending: React.FC<Props> = React.memo(({ item}) => {
+  return (
+    <ThemedView className="items-center" style={styles.card}>
+      <Image
+        source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
+        style={styles.poster}
+      />
+      <ThemedText type="link" className="text-center" style={styles.title}>
+        {"original_title" in item ? item.original_title : item.original_name}
+      </ThemedText>
+    </ThemedView>
+  );
+});
 
 const styles = StyleSheet.create({
   card: {
