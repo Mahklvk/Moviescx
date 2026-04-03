028cc58cb276486af070644344d51c6a6a9b3044
Mahklvk <ranggasidikp@proton.me>
2026-04-03 11:06:43 +0700

    change: dynamic fetching for render filter

--- src/api/trending.ts
+++ src/api/trending.ts
@@ -1,27 +1,9 @@
 import { Interceptor } from "./interceptor";
 
 export const Trending = {
-  All: async (time_window: "day" | "week") => {
+  Trending: async (time_window: "day" | "week", data: "movie" | "tv") => {
     try {
-      const response = await Interceptor().get(`/3/trending/all/${time_window}`);
-      return response.data;
-    } catch (error) {
-      throw new Error("Failed to fetch movie");
-    }
-  },
-
-  Tv: async (time_window: "day" | "week") => {
-    try {
-      const response = await Interceptor().get(`/3/trending/tv/${time_window}`);
-      return response.data;
-    } catch (error) {
-      throw new Error("Failed to fetch movie");
-    }
-  },
-
-  Movies: async (time_window: "day" | "week") => {
-    try {
-      const response = await Interceptor().get(`/3/trending/movie/${time_window}`);
+      const response = await Interceptor().get(`/3/trending/${data}/${time_window}`);
       return response.data;
     } catch (error) {
       throw new Error("Failed to fetch movie");
