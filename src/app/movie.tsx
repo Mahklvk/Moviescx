e5a14b60915c38b405a1918bb5c5628ca4dd1417
Mahklvk <ranggasidikp@proton.me>
2026-04-13 07:06:21 +0700

    add: movie menu

---
+++ src/app/movie.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import { ThemedText } from '@/components/themed-text';
+
+export default function Movies() {
+  return (
+    <View>
+      <ThemedText>
+        test
+      </ThemedText>
+    </View>
+  )
+}
