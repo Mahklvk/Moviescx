2edeb2ed4eda10ed9ea5f2d17e702e728334bba8
Mahklvk <ranggasidikp@proton.me>
2026-04-16 23:03:36 +0700

    change: carousel and image size

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -2,13 +2,13 @@ import { TopRated } from "@/api/top-rated";
 import Tv from "@/interface/tv";
 import { useQuery } from "@tanstack/react-query";
 import { useCallback } from "react";
+import { View } from "react-native";
 import { useSharedValue } from "react-native-reanimated";
 import Carousel from "react-native-reanimated-carousel";
-import { View } from "react-native";
-import { RenderTopRated } from "./render-top-rated";
+import { RenderTopRated } from "./renderer/render-top-rated";
 
 export function Carousels() {
-  const progress = useSharedValue<number>(0);
+  const scrollOffsetValue = useSharedValue<number>(0);
 
   const { data: topRate = [] } = useQuery({
     queryKey: ["topRated"],
@@ -24,21 +24,14 @@ export function Carousels() {
   return (
     <View>
       <Carousel
-        width={400}
-        height={300}
-        autoPlayInterval={2000}
+        width={410}
+        height={150}
+        autoPlayInterval={1000}
         data={topRate}
         loop={true}
         pagingEnabled={true}
         snapEnabled={true}
-        mode="parallax"
-        modeConfig={{
-          parallaxScrollingScale: 0.9,
-          parallaxScrollingOffset: 50,
-        }}
-        onProgressChange={(offsetProgress, absoluteProgress) => {
-          progress.value = absoluteProgress;
-        }}
+        onSnapToItem={(index: number) => index}
         renderItem={renderTopRated}
       />
     </View>
