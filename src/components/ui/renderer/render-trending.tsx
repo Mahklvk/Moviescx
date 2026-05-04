1dcae721655ce321657acdf0588ba1dec888e5a8
Mahklvk <ranggasidikp@proton.me>
2026-05-04 18:46:24 +0700

    feat: delete filter weekly and daily, add genre badge, bookmark, and overview

--- src/components/ui/renderer/render-trending.tsx
+++ src/components/ui/renderer/render-trending.tsx
@@ -1,55 +1,106 @@
+import Genre from "@/interface/genre";
 import Movie from "@/interface/movies";
-import Tv from "@/interface/tv";
+import Feather from "@expo/vector-icons/Feather";
 import { Image } from "expo-image";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { ThemedText } from "../../themed-text";
 import { ThemedView } from "../../themed-view";
-import CardSkeleton  from "../skeleton";
-type Media = Movie | Tv;
+import { Genres } from "../genre";
+import CardSkeleton from "../skeleton";
 
 type Props = {
-  item: Media;
+  item: Movie;
+  genres: Genre[];
   isLoading?: boolean;
 };
-
 export const RenderTrending: React.FC<Props> = React.memo(
-  ({ item, isLoading }) => {
+  ({ item, genres, isLoading }) => {
     if (isLoading) {
-      return <CardSkeleton type="trending"/>;
+      return <CardSkeleton type="trending" />;
     }
-  return (
-    
-    <ThemedView className="items-center" style={styles.card}>
-      <Image
-        source={{ uri: `https://image.tmdb.org/t/p/w500${item.poster_path}` }}
-        style={styles.poster}
-        alt={"original_title" in item ? item.title : item.name}
-        
-        />
-      <ThemedText type="link" className="text-center" style={styles.title}>
-        {"original_title" in item ? item.title : item.name}
-      </ThemedText>
+
+    return (
+      <ThemedView className="m-5 border-2 border-[#89b4fa] rounded-xl">
+        <View className="absolute top-2 left-2 z-40">
+          <TouchableOpacity>
+            <Feather name="bookmark" size={34} color={"#89b4fa"} />
+          </TouchableOpacity>
+        </View>
+
+        <View className="absolute right-[-10px] z-40">
+          <View className="border border-[#89b4fa] rounded-full px-2 py-1 bg-[#b4befe]">
+            <ThemedText
+              style={{
+                color: "#24273a",
+              }}
+            >
+              {item.vote_average.toFixed(1)}
+            </ThemedText>
+          </View>
+        </View>
+        <View className="items-center">
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+            }}
+            style={styles.poster}
+            alt={item.title}
+          />
+          <ThemedText style={styles.title} numberOfLines={1} className="my-2">
+            {item.title}
+          </ThemedText>
+          <View style={styles.overview} className="border-t-2 border-[#89b4fa]">
+            <ThemedText
+              type="small"
+              numberOfLines={2}
+              className="text-justify me-5 ms-5"
+            >
+              {item.overview}
+            </ThemedText>
+          </View>
+          <Genres genres={genres} item={item} />
+        </View>
       </ThemedView>
-  );
-});
+    );
+  },
+);
 
 const styles = StyleSheet.create({
-  card: {
-    alignItems: "center",
-    margin: 20,
-    borderColor: "#89b4fa",
-    borderWidth: 2,
-    borderRadius:10,
-  },
   poster: {
-    width: 150,
-    height: 200,
+    width: 200,
+    height: 300,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
   },
   title: {
-    width: 140,
+    width: 200,
     fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  genreContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: 4,
+    marginTop: 4,
+  },
+
+  badge: {
+    borderWidth: 1,
+    borderColor: "#89b4fa",
+    backgroundColor: "#1e2030",
+    paddingHorizontal: 6,
+    paddingVertical: 2,
+    marginBottom: 5,
+  },
+
+  badgeText: {
+    fontSize: 10,
+  },
+
+  overview: {
+    width: 200,
   },
 });
--- src/components/ui/trending.tsx
+++ src/components/ui/trending.tsx
@@ -1,61 +1,34 @@
-import { Trending } from "@/api/trending";
+import { apiMovie } from "@/api/movies";
 import Movie from "@/interface/movies";
-import Tv from "@/interface/tv";
 import { useQuery } from "@tanstack/react-query";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { FlatList, TouchableOpacity, View } from "react-native";
 import { ThemedText } from "../themed-text";
 import { RenderTrending } from "./renderer/render-trending";
 
-type Media = Movie | Tv;
-
 export function Trendings() {
-  const [filterTime, setFilterTime] = useState<"day" | "week">("day");
-  const [filterTrending, setFilterTrending] = useState<"movie" | "tv">("movie");
 
   const { data: trending = [], isLoading } = useQuery({
-    queryKey: ["tv", filterTime, filterTrending],
-    queryFn: () => Trending.Trending(filterTime, filterTrending),
+    queryKey: ["trending"],
+    queryFn: () => apiMovie.Trending(),
     select: (data) => data.results,
-    staleTime: 1000 * 60 * 5, // cache 5 menit
+    staleTime: 1000 * 60 * 5,
   });
+
+  const { data: genreData } = useQuery({
+  queryKey: ["genres",],
+  queryFn: () => apiMovie.Genres(),
+  staleTime: 1000 * 60 * 5, 
+});
 
   return (
     <View>
-      <View style={{ flexDirection: "row", margin: 10, alignItems: "center" }}>
-        {/* Kanan: Day / Week */}
-        <View style={{ flexDirection: "row", gap: 10, marginRight: 10 }}>
-          {["day", "week"].map((item) => (
-            <TouchableOpacity
-              key={item}
-              onPress={() => setFilterTime(item as "day" | "week")}
-              style={{
-                paddingHorizontal: 12,
-                paddingVertical: 6,
-                backgroundColor:
-                  filterTime === item ? "transparent" : "#b4befe",
-                borderColor: "#89b4fa",
-                borderWidth: 2,
-                borderRadius: 10,
-              }}
-            >
-              <ThemedText
-                style={{
-                  color: filterTime === item ? "#cad3f5" : "#24273a",
-                }}
-              >
-                {item.toUpperCase()}
-              </ThemedText>
-            </TouchableOpacity>
-          ))}
-        </View>
-      </View>
       <FlatList
         className="item-center"
         data={isLoading ? Array(5).fill({}) : trending}
         keyExtractor={(_, index) => index.toString()}
         renderItem={({ item }) => (
-          <RenderTrending item={item as Media} isLoading={isLoading} />
+          <RenderTrending item={item as Movie} genres={genreData?.genres} isLoading={isLoading} />
         )}
         indicatorStyle="white"
         initialNumToRender={3}
