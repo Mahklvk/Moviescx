30678db78c8b93562bf9403254e134c6ac142c81
Mahklvk <ranggasidikp@proton.me>
2026-06-19 16:54:59 +0700

    feat: add top rated movie, detail, credits, wathproviders, reviews

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -3,11 +3,23 @@ type SearchMoviesParams = {
   query: string;
   page?: number;
 };
+import { WatchProvider } from "@/interface/streaming";
 export const apiMovie = {
   Trending: async () => {
     try {
       const response = await Interceptor().get(
         `/3/trending/movie/day`,
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to fetch movie");
+    }
+  },
+
+  TopRated: async () => {
+    try {
+      const response = await Interceptor().get(
+        '/3/movie/top_rated'
       );
       return response.data;
     } catch (error) {
@@ -57,6 +69,42 @@ export const apiMovie = {
       return response.data;
     } catch (error) {
       throw new Error('gagal ambil data njir')
+    }
+  }, 
+
+  Detail: async (movie_id: number) => {
+    try {
+      const response = await Interceptor().get(`/3/movie/${movie_id}`)
+      return response.data;
+    } catch (error) {
+      throw new Error('gagal ambil  data ')
+    }
+  },
+
+   Credits: async (movie_id: number) => {
+    try {
+      const response = await Interceptor().get(`/3/movie/${movie_id}/credits`)
+      return response.data;
+    } catch (error) {
+      throw new Error('gagal ambil  data ')
+    }
+  },
+   
+  WatchProviders: async (movie_id: number): Promise<WatchProvider> => {
+  try {
+    const response = await Interceptor().get(`/3/movie/${movie_id}/watch/providers`);
+    return response.data;
+  } catch (error) {
+    throw new Error("gagal ambil data watch providers");
+  }
+  },
+  
+  Reviews: async (movie_id: number) => {
+    try {
+      const response = await Interceptor().get(`/3/movie/${movie_id}/reviews`)
+      return response.data;
+    } catch (error) {
+      throw new Error('gagal ambil  data ')
     }
   }
 };
