4c70935310ff4099abb7c909ee5d5194f4dfcc11
Mahklvk <ranggasidikp@proton.me>
2026-06-19 16:59:04 +0700

    feat: expo router

--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useColorScheme } from "react-native";
 
 import { Colors } from "@/constants/theme";
+import { Stack } from "expo-router";
 
 export default function AppTabs() {
   const scheme = useColorScheme();
