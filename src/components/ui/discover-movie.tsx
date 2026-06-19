6623cabe61c819d637de62ecb9d89191d2580c2a
Mahklvk <ranggasidikp@proton.me>
2026-06-19 17:02:46 +0700

    feat: fix ui discover, and now discover dissapear when search has keyword

--- src/components/ui/discover-movie.tsx
+++ src/components/ui/discover-movie.tsx
@@ -1,10 +1,11 @@
 import { Discover } from "@/api/discovery";
 import { apiMovie } from "@/api/movies";
+import { SearchMovies } from "@/components/ui/search";
 import Movie from "@/interface/movies";
 import { useQuery } from "@tanstack/react-query";
-import { FlatList, View } from "react-native";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { RenderDiscoverMovie } from "./renderer/render-discover-movie";
-
 export function DiscoverMovie() {
   const { data: discover = [], isLoading } = useQuery({
     queryKey: ["discover"],
@@ -19,24 +20,26 @@ export function DiscoverMovie() {
     staleTime: 1000 * 60 * 5,
   });
 
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
-    <View>
-      <FlatList
-        className="item-center"
-        data={isLoading ? Array(5).fill({}) : discover}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={({ item }) => (
-          <RenderDiscoverMovie
-            item={item as Movie}
-            genres={genreData?.genres}
-            isLoading={isLoading}
-          />
-        )}
-        indicatorStyle="white"
-        initialNumToRender={3}
-        numColumns={2}
-        removeClippedSubviews={true}
-      />
-    </View>
+    <>
+      <SearchMovies searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+
+      {!searchQuery && (
+        <FlatList
+          data={isLoading ? Array(6).fill({}) : discover}
+          keyExtractor={(_, index) => index.toString()}
+          numColumns={2}
+          renderItem={({ item }) => (
+            <RenderDiscoverMovie
+              item={item as Movie}
+              genres={genreData?.genres}
+              isLoading={isLoading}
+            />
+          )}
+        />
+      )}
+    </>
   );
 }
--- src/components/ui/renderer/render-discover-movie.tsx
+++ src/components/ui/renderer/render-discover-movie.tsx
@@ -2,13 +2,13 @@ import Genre from "@/interface/genre";
 import Movie from "@/interface/movies";
 import Feather from "@expo/vector-icons/Feather";
 import { Image } from "expo-image";
+import { useRouter } from "expo-router";
 import React from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { ThemedText } from "../../themed-text";
 import { ThemedView } from "../../themed-view";
 import { Genres } from "../genre";
 import CardSkeleton from "../skeleton";
-
 type Props = {
   item: Movie;
   genres: Genre[];
@@ -20,38 +20,45 @@ export const RenderDiscoverMovie: React.FC<Props> = React.memo(
       return <CardSkeleton type="trending" />;
     }
 
-    return (
-      <ThemedView className="m-5 border-2 border-[#89b4fa] rounded-xl" style={styles.container}>
-        <View className="absolute top-2 left-2 z-40">
-          <TouchableOpacity>
-            <Feather name="bookmark" size={34} color={"#89b4fa"} />
-          </TouchableOpacity>
-        </View>
+    const handlePress = (movie_id: number) => {
+      router.push(`/detailMovie?movie_id=${movie_id}`);
+    };
 
-        <View className="absolute right-[-10px] z-40">
-          <View className="border border-[#89b4fa] rounded-full px-2 py-1 bg-[#b4befe]">
-            <ThemedText
-              style={{
-                color: "#24273a",
-              }}
-            >
-              {item.vote_average.toFixed(1)}
-            </ThemedText>
+    const router = useRouter();
+    return (
+      <ThemedView className="m-4 border-2 border-[#89b4fa] rounded-xl">
+        <TouchableOpacity onPress={() => handlePress(item.id)}>
+          <View className="absolute top-2 left-2 z-40">
+            <TouchableOpacity>
+              <Feather name="bookmark" size={34} color={"#89b4fa"} />
+            </TouchableOpacity>
           </View>
-        </View>
-        <View className="items-center">
-          <Image
-            source={{
-              uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
-            }}
-            style={styles.poster}
-            alt={item.title}
-          />
-          <ThemedText style={styles.title} numberOfLines={2}>
-            {item.title}
-          </ThemedText>
-          <Genres genres={genres} item={item} />
-        </View>
+
+          <View className="absolute right-[-10px] z-40">
+            <View className="border border-[#89b4fa] rounded-full px-2 py-1 bg-[#b4befe]">
+              <ThemedText
+                style={{
+                  color: "#24273a",
+                }}
+              >
+                {item.vote_average.toFixed(1)}
+              </ThemedText>
+            </View>
+          </View>
+          <View className="items-center">
+            <Image
+              source={{
+                uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+              }}
+              style={styles.poster}
+              alt={item.title}
+            />
+            <ThemedText style={styles.title} numberOfLines={1}>
+              {item.title}
+            </ThemedText>
+            <Genres genres={genres} item={item} />
+          </View>
+        </TouchableOpacity>
       </ThemedView>
     );
   },
@@ -59,8 +66,8 @@ export const RenderDiscoverMovie: React.FC<Props> = React.memo(
 
 const styles = StyleSheet.create({
   poster: {
-    width: 150,
-    height: 200,
+    width: 175,
+    height: 300,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
   },
@@ -70,9 +77,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 
-  container: {
-    width:155
-  },
   genreContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
