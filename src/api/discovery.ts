68f9b8fb29c1c9ba50f1ef2be22f0fa16571571e
Mahklvk <ranggasidikp@proton.me>
2026-06-19 16:53:44 +0700

    feat: Discovery adult content = false

--- src/api/discovery.ts
+++ src/api/discovery.ts
@@ -3,7 +3,7 @@ import { Interceptor } from "./interceptor";
 export const Discover = {
   Movies: async () => {
     try {
-      const response = await Interceptor().get("/3/discover/movie");
+      const response = await Interceptor().get("/3/discover/movie?include_adult=false");
       return response.data;
     } catch (error) {
       throw new Error("Failed to fetch movie");
